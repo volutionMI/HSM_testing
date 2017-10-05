3ebff3e645a651b5173ce2a01006dce205922b81
volution <mbuecking@thinkvolution.com>
2017-10-05 15:59:01 -0400

    workflow child theme

---
+++ wp-content/themes/twentysixteen/page-LF2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: LookFeel_2
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header('2'); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'smenu2' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
