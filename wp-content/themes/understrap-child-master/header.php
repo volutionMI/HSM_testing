6f66eadf290fe63cf7567f3425304612bfe6eff5
volution <mbuecking@thinkvolution.com>
2018-03-29 16:21:24 -0400

    screwing around trying to get the blog info to center
    seems like the media query is over riding the smaller version even
    though other items are working
    
    shame not css could that be the problem but the main menu items seem to
    work.

--- wp-content/themes/understrap-child-master/header.php
+++ wp-content/themes/understrap-child-master/header.php
@@ -45,32 +45,30 @@ wp_nav_menu( array(
 ?>
 	</div>
 	<div class="row white-menu">
-<div class="logo-brand">
-		<?php endif; ?>
+		<div class="logo-brand">
+			<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+			<!-- Your site title as branding in the menu -->
+			<?php if ( ! has_custom_logo() ) { ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+				<?php if ( is_front_page() && is_home() ) : ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-							
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-						
-						<?php endif; ?>
-						
+					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 					
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo start MRB-->
+				<?php else : ?>
 
-		</div>
-<div class="bloginfo"><h1><?php bloginfo( 'name' ); ?></h1><p><?php bloginfo( 'description' ); ?>
-</div>
-		
-</div>
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+				
+				<?php endif; ?>
+				
+			
+			<?php } else {
+				the_custom_logo();
+			} ?><!-- end custom logo start MRB-->
+		</div>	
+		<div class="bloginfo"><h1><?php bloginfo( 'name' ); ?></h1><p><?php bloginfo( 'description' ); ?>
+		</div>		
+	</div>
 	<nav class="navbar navbar-expand-md navbar-dark"><!-- end MRB-->			
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
