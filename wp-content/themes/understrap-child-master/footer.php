496ebf5440d487283a1f3dfdf204e0e43b3f29a7
volution <mbuecking@thinkvolution.com>
2018-03-29 15:36:47 -0400

    return to footer and nav media query

--- wp-content/themes/understrap-child-master/footer.php
+++ wp-content/themes/understrap-child-master/footer.php
@@ -16,34 +16,115 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="wrapper" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-md-12"><?php get_sidebar('footerfull'); ?>
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
+		    			<div class="row footer_menu">
+				<div class="col-md-5 widget">
+        	    <h2>Article</h2>
+                <article class="widget_content">
+                    <ul>
+                    	<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                    	<li id="menu-item-345" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-345 dropdown"><a title="Michigan History Day®" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle">Michigan History Day<sup style="size: 50%;">®</sup> <span class="caret"></span></a>
+<ul class=" dropdown-menu" role="menu">
+	<li id="menu-item-494" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-494"><a title="Michigan History Day ®" href="http://localhost:8888/mhd/" class="nav-link">Michigan History Day ®</a></li>
+</ul>
+</li>
+						<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                 		<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                    	<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                    	<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                 	</ul>
+                 	<ul>
+                    	<li id="menu-item-222" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-222 dropdown"><a title="Michigan History magazine" href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"><em>Michigan History</em> magazine <span class="caret"></span></a>
+							<ul class=" dropdown-menu" role="menu">
+								<li id="menu-item-407" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-407"><a title="All about Michigan History magazine" href="http://localhost:8888/michigan-history-magazine-2/" class="nav-link">All about <em>Michigan History</em> magazine</a></li>
+								<li id="menu-item-403" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-403"><a title="Subscribe" href="http://localhost:8888/subscribe/" class="nav-link">Subscribe</a></li>
+								<li id="menu-item-397" class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-397"><a title="Overview" href="http://localhost:8888/overview/" class="nav-link">Overview</a></li>
+								<li id="menu-item-408" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-408"><a title="Current Issue" href="http://localhost:8888/2018/01/22/current-issue/" class="nav-link">Current Issue</a></li>
+								<li id="menu-item-405" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-405"><a title="Content Sponsors" href="http://localhost:8888/2018/03/11/content-sponsors/" class="nav-link">Content Sponsors</a></li>
+								<li id="menu-item-406" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-406"><a title="Advertising" href="http://localhost:8888/2018/01/22/advertising/" class="nav-link">Advertising</a></li>
+								<li id="menu-item-409" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-409"><a title="HSM Magazines Writers Guidelines" href="http://localhost:8888/2018/01/22/writers-guidelines/" class="nav-link">HSM Magazines Writers Guidelines</a></li>
+								<li id="menu-item-404" class="menu-item menu-item-type-post_type menu-item-object-post nav-item menu-item-404"><a title="Wholesale" href="http://localhost:8888/2018/03/11/wholesale/" class="nav-link">Wholesale</a></li>
+							</ul>
+						</li>
+                 	</ul>
+                 </article>
+    		</div>
+            <div class="col-md-3 widget">
+        	    <h2>Link</h2>
+                <article class="widget_content">
+                    <ul>
+                        <li id="menu-item-402" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-210 current_page_item nav-item menu-item-402 active"><a href="http://localhost:8888/" class="nav-link"><i class="fa fa-home"></i></a></li>
+                 </ul>
+                 </article>
+    		</div>
+            <div class="col-md-4 widget">
+                <h2>Contact Us</h2>
+                <article class="widget_content">
+                    <p>Jl. Lingkar Selatan, Kasihan, Bantul<br> Yogyakarta Indonesia 55183<br>Phone: +62 274 387656</p>
+                </article>
+    		</div>
+    	</div>
+    </div>
+</div>
+<div class="footer-bottom">
+    <div class="container">
+    	<div class="col-md-12 copyright">
+    		<p>&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?></p><span class="pull-right">&nbsp;</span>
+        </div>
 
 	</div><!-- container end -->
 
@@ -52,7 +133,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
-
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script>$(document).ready(function() {	
+	$( ".widget h2" ).click(
+		function() {
+			$(this).parent().toggleClass('active');
+		}
+	);	  	
+});
+</script>
 </body>
 
 </html>
